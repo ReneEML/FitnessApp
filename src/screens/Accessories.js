7a36f580d039ca02cbbb934e3c9ecd3d313d937a
rlevesqu <rlevesqu@uwaterloo.ca>
2020-11-26 15:09:00 -0800

    update

---
+++ src/screens/Accessories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View, ScrollView, SafeAreaView} from 'react-native';
+
+const Accessories = () => {
+    return(
+        <SafeAreaView>
+            <ScrollView horizontal={true}/>
+        </SafeAreaView>
+        
+    )
+}
+
+const styles = StyleSheet.create(
+
+);
+export default Accessories;
